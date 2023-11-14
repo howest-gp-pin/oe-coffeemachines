74b8513d5ba9147dc6856019f0667a354c532258
Maxim Lesy <maxim.lesy@howest.be>
2023-11-14 15:45:13 +0100

    Use chart.js with jsinterop

---
+++ Pin.CoffeeMachines/wwwroot/js/script.js
@@ -0,0 +1,22 @@
+﻿window.displayChart = (labels, data) => {
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '# of machines',
+                data: data,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
